c1cf6af8ef1db4f33cb021b8b9197ba766d16b14
Mirko <xiangormirko@gmail.com>
2014-03-31 19:24:29 -0400

    commit

--- Global Brigades/profileViewController.h
+++ Global Brigades/profileViewController.h
@@ -7,7 +7,28 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <Parse/Parse.h>
 
 @interface profileViewController : UIViewController
+
+@property (nonatomic, strong) NSArray *name;
+
+@property (weak, nonatomic) IBOutlet UILabel *nameLabel;
+
+@property (weak, nonatomic) IBOutlet UILabel *brigadeLabel;
+
+@property (weak, nonatomic) IBOutlet UILabel *chapterLabel;
+
+
+@property (weak, nonatomic) IBOutlet UITextView *profileSummary;
+
+@property (nonatomic, copy) NSString *hello;
+
+
+
+- (IBAction)updateProfile:(id)sender;
+
+
+
 
 @end
--- Global Brigades/profileViewController.m
+++ Global Brigades/profileViewController.m
@@ -10,21 +10,64 @@
 
 @interface profileViewController ()
 
+
+
 @end
 
+
 @implementation profileViewController
+
 
 
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    
 	// Do any additional setup after loading the view.
+    PFQuery *query= [PFUser query];
+    [query whereKey:@"objectId" equalTo:[[PFUser currentUser] objectId]];
+    [query getObjectInBackgroundWithId:[[PFUser currentUser] objectId] block:^(PFObject *userInfo, NSError *error) {
+        // Do something with the returned PFObject in the gameScore variable.
+        NSLog(@"%@", userInfo);
+        
+        NSString *username= userInfo[@"username"];
+        NSString *brigade= userInfo[@"brigade"];
+        NSString *chapter= userInfo[@"chapter"];
+        
+        self.nameLabel.text=username;
+        self.brigadeLabel.text=brigade;
+        self.chapterLabel.text=chapter;
+        
+    }];
+    
+    PFQuery *query1 = [PFQuery queryWithClassName:@"userInfo"];
+    [query1 whereKey:@"parent" equalTo:[[PFUser currentUser] objectId]];
+    [query1 findObjectsInBackgroundWithBlock:^(NSArray *objects, NSError *error) {
+        
+        if(objects.count != 0){
+        PFObject * object = objects[0];
+        self.profileSummary.text=  object[@"summary"];
+        }
+        else
+        {
+            self.profileSummary.text=@"Insert your personal summary";
+        }
+    }];
+    
+
 }
 
 -(void) viewWillAppear:(BOOL)animated{
     [super viewWillAppear:animated];
     [self.navigationController.navigationBar setHidden:YES];
+    
+
+
+    
+    
+    
+
 }
 
 - (void)didReceiveMemoryWarning
@@ -44,4 +87,44 @@
 
 
 
+
+#pragma mark- profile summary
+
+- (IBAction)updateProfile:(id)sender {
+    
+
+    PFQuery *query1 = [PFQuery queryWithClassName:@"userInfo"];
+    [query1 whereKey:@"parent" equalTo:[[PFUser currentUser] objectId]];
+    [query1 getFirstObjectInBackgroundWithBlock:^(PFObject *userInfo, NSError *error) {
+    
+        if(userInfo != nil){
+            
+            NSString *summary= self.profileSummary.text;
+            userInfo[@"summary"]= summary;
+            [userInfo saveInBackground];
+        }
+        else
+        {
+            NSString *summary= self.profileSummary.text;
+            
+            PFObject *userInfo= [PFObject objectWithClassName:@"userInfo"];
+            
+            userInfo[@"summary"]=summary;
+            
+            userInfo[@"parent"]=[[PFUser currentUser]objectId];
+            
+            
+            [userInfo saveInBackground];
+        }
+        
+        
+        
+    }];
+
+        
+
+
+
+    
+}
 @end
