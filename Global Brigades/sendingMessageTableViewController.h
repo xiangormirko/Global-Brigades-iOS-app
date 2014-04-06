dbc83d08e007aff8d602812d7d121fd00d770215
Mirko <xiangormirko@gmail.com>
2014-04-06 14:03:12 -0400

    commit

---
+++ Global Brigades/sendingMessageTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  sendingMessageTableViewController.h
+//  Global Brigades
+//
+//  Created by MIRKO on 4/6/14.
+//  Copyright (c) 2014 Xiangormirko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface sendingMessageTableViewController : UITableViewController
+
+@end
