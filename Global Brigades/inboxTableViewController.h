9c51ed5dd67b889db35faf8e835b00532f0a0668
Mirko <xiangormirko@gmail.com>
2014-04-13 13:42:21 -0400

    inbox shows jambe

---
+++ Global Brigades/inboxTableViewController.h
@@ -0,0 +1,14 @@
+//
+//  inboxTableViewController.h
+//  Global Brigades
+//
+//  Created by MIRKO on 4/13/14.
+//  Copyright (c) 2014 Xiangormirko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface inboxTableViewController : UITableViewController
+@property (nonatomic, strong) NSArray *messages;
+
+@end
